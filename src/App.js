81d0b8cb9e729acde88c85d92767578f58e6bbc0
Oloniyo Bolaji <ayoolabolaji12@yahoo.com>
2025-01-10 19:06:11 +0100

    responsiveness adjusted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState} from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
